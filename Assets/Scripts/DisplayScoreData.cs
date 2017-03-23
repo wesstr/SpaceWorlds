d762fa0e414ab69ae69b1eb27f7efcac2e989b69
Wesley Strong <Wesley@DESKTOP-CJRJMQ2.(none)>
2017-03-23 15:36:23 -0500

    Twiked UI

---
+++ Assets/Scripts/DisplayScoreData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScoreData : MonoBehaviour {
+
+    public GameObject m_NamePrefab;
+    public GameObject m_ScorePrefab;
+    public GameObject m_NameContainer;
+    public GameObject m_ScoreContainer;
+    public GameManager m_GameManager;
+
+    private Text m_NameText;
+    private Text m_ScoreText;
+    private GameObject m_JustMade;
+    private List<string> m_Names;
+    private List<int> m_Scors;
+
+	void Start () {
+        m_Names = m_GameManager.GetNames();
+        m_Scors = m_GameManager.GetScors();
+
+        Debug.Log(m_Names.Count);
+
+        for (int i = 0; m_Scors.Count != i; i++)
+        {
+            m_JustMade = Instantiate(m_NamePrefab);
+            m_JustMade.GetComponent<Text>().text = m_Names[i] + " >";
+            m_JustMade.transform.parent = m_NameContainer.transform;
+            m_JustMade = Instantiate(m_ScorePrefab);
+            m_JustMade.GetComponent<Text>().text = "Score: " + m_Scors[i];
+            m_JustMade.transform.parent = m_ScoreContainer.transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,9 +18,14 @@ public class GameManager : MonoBehaviour {
     public static string m_PlayerName;
     public int m_LastScore;
 
+    private ScoreData m_ScoreData;
+
 	void Awake () {
         m_Score = 0;
         m_Health = 100;
+        m_ScoreData = LoadUserData();
+        if (m_ScoreData == null)
+            Debug.Log("Failed to load score data!");
 	}
 	
 	// Update is called once per frame
@@ -33,31 +38,47 @@ public class GameManager : MonoBehaviour {
         }
 	}
 
+    public List<String> GetNames()
+    {
+        return m_ScoreData.names;
+    }
+
+    public List<int> GetScors()
+    {
+        return m_ScoreData.scores;
+    }
+
     public void Save(string name)
     {
         m_PlayerName = name;
         BinaryFormatter bf = new BinaryFormatter();
         FileStream file = File.Open(Application.persistentDataPath + "/playerScores.dat", FileMode.Append);
+        Debug.Log(Application.persistentDataPath);
+        //ScoreData data = new ScoreData();
+        m_ScoreData.names.Insert(0, m_PlayerName);
+        m_ScoreData.scores.Insert(0, m_Score);
+        Debug.Log(m_ScoreData.scores.Count);
 
-        ScoreData data = new ScoreData();
-        data.names.Insert(0, m_PlayerName);
-        data.scores.Insert(0, m_Score);
-
-        bf.Serialize(file, data);
+        bf.Serialize(file, m_ScoreData);
         file.Close();
     }
 
-    void LoadUserData()
+    ScoreData LoadUserData()
     {
         if (File.Exists(Application.persistentDataPath + "/playerScores.dat"))
         {
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Open(Application.persistentDataPath + "/playerScores.dat", FileMode.Open);
-            ScoreData data = (ScoreData)bf.Deserialize(file);
+            ScoreData data = (ScoreData) bf.Deserialize(file);
             file.Close();
 
-            m_LastScore = data.scores[0];
-            m_PlayerName = data.names[0];
+            //m_LastScore = data.scores[0];
+            //m_PlayerName = data.names[0];
+            return data;
+        }
+        else
+        {
+            return null;
         }
     }
 }
